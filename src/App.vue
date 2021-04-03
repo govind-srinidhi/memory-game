a9344ed96c7f3d5d320593e4059e3e5516823fe6
Govind Srinidhi Dayal <govindsd@peopleintouch.com>
2021-04-03 14:14:46 +0200

    memory-game: added application structure

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+component(:is="layout")
+  router-view
+</template>
+
+<script>
+import DefaultLayout from "./layouts/default";
+
+export default {
+  name: "MemoryGame",
+
+  components: {
+    DefaultLayout,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
