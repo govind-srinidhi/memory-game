5f6c5f96982f5d2afbca11f769173a8b11e17a49
Govind Srinidhi Dayal <govindsd@peopleintouch.com>
2021-04-05 10:57:41 +0200

    memory-game: included blank layout

---
+++ src/layouts/blank/index.vue
@@ -0,0 +1,6 @@
+<!--
+  It contains vue component for blank layout.
+-->
+<template lang = "pug" src = "./template.pug"></template>
+<script src = "./script.js"></script>
+<style lang = "sass" src = "./style.sass"/>
