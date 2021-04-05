53497777bd769fa6096dd59dab77f618879a55bc
Govind S <25.govind@gmail.com>
2021-04-05 17:56:53 +0200

    memory-game: added vuex package

--- src/main.js
+++ src/main.js
@@ -2,15 +2,18 @@
  * @file This is the entry file for memory game application.
  * All the plugins should be loaded here before the new vue instance is created and mounted.
  */
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify';
-import router from './plugins/router';
+import Vue from "vue"
+import App from "@/App.vue"
+import "@/plugins/constants"
+import vuetify from "@/plugins/vuetify"
+import router from "@/plugins/router"
+import store from "@/plugins/vuex"
 
 Vue.config.productionTip = false
 
 new Vue({
-  vuetify,
-  router,
-  render: h => h(App)
+ vuetify,
+ router,
+ store,
+ render: h => h(App)
 }).$mount('#app')
