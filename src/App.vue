dd04164ee871bae088ebe48d25ce725ebd91a89f
Govind Srinidhi Dayal <govindsd@peopleintouch.com>
2021-04-05 11:03:17 +0200

    memory-game: included routing for the app

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,23 @@ component(:is="layout")
 </template>
 
 <script>
+import BlankLayout from "./layouts/blank";
 import DefaultLayout from "./layouts/default";
 
 export default {
   name: "MemoryGame",
-
   components: {
+    BlankLayout,
     DefaultLayout,
   },
-
-  data: () => ({
-    //
-  }),
+  computed: {
+    layout() {
+      let layout = this.$route.meta.layout;
+      if (layout) {
+        return `${layout}Layout`;
+      }
+      return "BlankLayout";
+    },
+  },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
+/**
+ * @file This is the entry file for memory game application.
+ * All the plugins should be loaded here before the new vue instance is created and mounted.
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import router from './plugins/router';
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
