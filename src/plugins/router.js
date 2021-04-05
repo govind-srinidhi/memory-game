d32248db9ee1ad6b451b81df933803e0e2fe49ba
Govind Srinidhi Dayal <govindsd@peopleintouch.com>
2021-04-05 11:05:47 +0200

    memory-game: added router and middleware

---
+++ src/plugins/router.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import routeCheck from "@/middleware/routeCheck";
+
+Vue.use(Router)
+
+const router = new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "*",
+      redirect: "/not-found"
+    }, {
+      path: "/not-found",
+      name: "not-found",
+      meta: {
+        layout: "Blank"
+      },
+      component: () => import(/* webpackChunkName: "not-found" */ "@/views/not-found")
+    }, {
+      path: "/",
+      name: "home",
+      meta: {
+        layout: "Default"
+      },
+      component: () => import(/* webpackChunkName: "home" */ "@/views/home")
+    }, {
+      path: "/results",
+      name: "results",
+      meta: {
+        layout: "Blank"
+      },
+      component: () => import(/* webpackChunkName: "home" */ "@/views/results")
+    }
+  ]
+})
+
+router.beforeEach(routeCheck);
+export default router
